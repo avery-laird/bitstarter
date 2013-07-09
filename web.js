6f4cef5eafd46dc75075c4da61d13c04ef228fdc
Ubuntu <ubuntu@ip-172-31-44-68.us-west-2.compute.internal>
2013-07-09 21:29:33 +0000

    changed web.js

--- web.js
+++ web.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var fs = require('fs');
-var buffer = new Buffer
+var buffer = new Buffer(24)
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
