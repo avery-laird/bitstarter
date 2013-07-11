63d7637f1006a8904f25c03a1bdae7fb30d20ec1
Ubuntu <ubuntu@ip-172-31-44-68.us-west-2.compute.internal>
2013-07-11 01:48:26 +0000

    got it written to files

--- grader.js
+++ grader.js
@@ -24,6 +24,8 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+var sys = require('util');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -65,10 +67,29 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <html_url>', 'Url to index.html')
         .parse(process.argv);
+    if(program.url) {
+    
+         
+      rest.get(program.url).on('complete', function(data) {      
+        fs.writeFile('tmphtml.html', data, function(err) {
+          if (err) throw err;
+          console.log('Wrote tmp file');
+        });
+      });
+
+   // var checkJson = checkHtmlFile('tmphtml.html', program.checks);
+   // var outJson = JSON.stringify(checkJson, null, 4);
+   // console.log(outJson);    
+   // fs.unlink('tmphtml.html');
+
+    } else {
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
+    }
+   
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
