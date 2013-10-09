90c1ba2102bcc4829070e91c75b0e381966d985f
Avery Laird <laird.avery@gmail.com>
2013-10-08 17:50:30 -0700

    created custom node server

---
+++ server.js
@@ -0,0 +1,10 @@
+var http = require('http'),
+    static = require('node-static');
+
+var folder = new(static.Server)('./img');
+
+http.createServer(function (req, res) {
+    req.addListener('end', function () {
+        folder.serve(req, res);
+    });
+}).listen(5000);
