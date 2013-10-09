b23df6f1b196b1bf9510bf33562353174470e1c8
Avery Laird <laird.avery@gmail.com>
2013-10-08 17:54:56 -0700

    tweaked server

--- web.js
+++ web.js
@@ -15,3 +15,15 @@ var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+var http = require('http'),
+    static = require('node-static');
+
+var folder = new(static.Server)('./img');
+
+http.createServer(function (req, res) {
+    req.addListener('end', function () {
+        folder.serve(req, res);
+    });
+}).listen(5000);
+~                           
